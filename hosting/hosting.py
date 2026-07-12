6740bb8e539aa170922ef4d5efc45401fe7aa9cf
Sadashiv161 <bhattsadashiv161@gmail.com>
2026-07-12 01:29:52 +0000

    Remove Colab-only import from again hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,16 +1,13 @@
 from huggingface_hub import HfApi
 import os
 
+token = os.getenv("HF_TOKEN")  # please use your token
 
+api = HfApi(token=token)
 
-token=os.getenv("HF_TOKEN")   # please use your token
-
-api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="Wellness_Tourism_Package/deployment",     # the local folder containing your files
-    # replace with your repoid
-    repo_id="sadashivbhatt/Wellness_Tourism_Package",          # the target repo
-
-    repo_type="space",                      # dataset, model, or space
-    path_in_repo="",                          # optional: subfolder path inside the repo
+    folder_path="deployment",                        # the local folder containing your files
+    repo_id="sadashivbhatt/Wellness_Tourism_Package",  # the target repo
+    repo_type="space",                                # dataset, model, or space
+    path_in_repo="",                                  # optional: subfolder path inside the repo
 )
