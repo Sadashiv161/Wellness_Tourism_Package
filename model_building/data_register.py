044bf958158ef2b2c6cafd6d0b51d1841543af24
Sadashiv161 <bhattsadashiv161@gmail.com>
2026-07-12 00:44:08 +0000

    Remove Colab-only import from data_register.py

--- model_building/data_register.py
+++ model_building/data_register.py
@@ -1,27 +1,35 @@
 
-from huggingface_hub import HfApi, create_repo
+
+from google.colab import userdata
+from huggingface_hub import HfApi
+
 from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+from huggingface_hub import HfApi, create_repo
 import os
 
 token = os.getenv("HF_TOKEN")
 
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
 repo_id = "sadashivbhatt/Wellness_Tourism_Package"    # please create your space and repository
+
 repo_type = "dataset"
 
 # Initialize API client
-api = HfApi(token=token)
+api = HfApi(token=os.getenv("HF_TOKEN"))
 
-# Step 1: Check if the dataset repo exists
+# Step 1: Check if the space exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Repo  '{repo_id}' already exists. Using it.")
+    print(f"Space '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
-    print(f"Repo '{repo_id}' not found. Creating new repo...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False, token=token)
-    print(f"Repo '{repo_id}' created.")
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
-    folder_path="data",
+    folder_path="Wellness_Tourism_Package/data",
     repo_id=repo_id,
     repo_type=repo_type,
 )
